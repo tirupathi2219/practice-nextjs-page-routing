8fe8a7af7e389a8af1bccfb7e2b9dd8c222174be
tirupathi2219 <tirupathi2219@gmail.com>
2023-06-02 14:18:43 +0530

    komuram-bheem

--- pages/blog/first.tsx
+++ pages/blog/first.tsx
@@ -1,7 +1,35 @@
 export default function First() {
     return (
         <>
-            <h1>First blog Page</h1>
+            <h1>First blog Page bheem</h1>
+            <p>
+                Komuram bheemudo komuram bheemudo korrasu negadole mandali koduko mandali koduko ooo....
+
+                Komuram bheemudo komuram bheemudo  raga raga sureedai ragalali koduko ragalali koduko oo...
+
+                kalmokta bhandchand ani vonigi togala karadavi talliki puttanattero putanatter oooo ...
+
+                julumu gaddeku talanu vonchi togala tudumu talli peguna  peraganattero peraganattero ooo...
+                
+                ..............komuram bheemudooo...........
+
+                charmamoliche debbaku abbantogala sinike raktam soosi sediri togala
+                gubulesi kanniru voliki togala
+                bhutalli sanubalu taganattero taganattero ooo..
+
+                ..............komuram bheemudooo...........
+
+                kaluvai pare nee gunde netturu ...uuuuuuuuuuu..
+                kaluvai pare nee gunde netturu nelamma nudhuti bottaitundi soodu ..
+                amma kalla paranaitundi soodu ..
+                talli pedavula navvai merisindi soodu ...
+
+                komuram bheemudo   komuram bheemudo pudami talliki janama aranamistiviro komuram bheemudo...............
+                ...............music...........
+
+
+
+            </p>
         </>
     )
 }
