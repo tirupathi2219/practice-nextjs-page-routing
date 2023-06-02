94b806edbe0be6d8fb8c8a6458dbea1310732df1
tirupathi2219 <tirupathi2219@gmail.com>
2023-06-02 14:01:03 +0530

    song-done

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,38 +1,45 @@
 export default function Blog() {
     return (
         <>
-        <h1>Blog Page</h1>
+            <h1>Blog Page</h1>
 
-        <p>
-            ye daredurina yetu veltundo adigana 
+            <p>
+                ye daredurina yetu veltundo adigana
+                yem tochani parugai pravahistu potunna
 
-em chustu unna nee vetikana edina 
-oorikine chuttu evevo kanipistu unna 
+                em chustu unna ney vetikana edina
+                oorikine chuttu evevo kanipistu potunna
 
-          kadalani o shilane ina 
-          trutilo karigey kalane ina
+                kadalani o shilane ina
+                trutilo karigey kalane ina
 
-     em teda undata nuvvevarantu adigitey nannevarina 
+                em teda undata nuvvevarantu adigitey nannevarina
 
-          illage kadadhaka o prshnai untanantunna 
-          edo oka badulai cherpoddanai kalannadugutu unna 
+                illage kadadhaka o prshnai untanantunna
+                edo oka badulai cherpoddanai kalannadugutu unna
 
-   naa venta padi nuv inta ontari anavoddu anoddu dayunchi evaru 
-    inkonni janmalaku saripadu srutulni itarulu erugaru 
+                naa venta padi nuv inta ontari anavoddu anoddu dayunchi evaru
+                inkonni janmalaku saripadu srutulni itarulu erugaru
 
-     naaa oopirini innalluga tana vennanti nadipina cheyuta evaridi
-      naa yedalayalo kusalam adigina ghuma ghumalevativii
+                naaa oopirini innalluga tana vennanti nadipina cheyuta evaridi
+                naa yedalayalo kusalam adigina gusa gusa kaburula  ghuma ghumalevarivee
+                ............................
 
-      udayam kagane tajaga pudutu unta kalam ipude nanu kanada
-      anagana antuntey unta tudi leni kadha nenuga 
+                udayam kagane tajaga pudutu unta kalam ipude nanu kanada
+                anagana antuntey unta yepudu purtavane avaka tudi leni kadha nenuga
 
-      gaali vatam laga aage alavate leka kalu nilvadu a chota 
-      nilakadaga a chirunama leka a bhadulu pondani leka enduku vestondo keka 
-      mounam ga .................
+                gaali vatam laga aage alavate leka kalu nilvadu a chota
+                nilakadaga ae chirunama leka a bhadulu pondani leka enduku vestondo keka
+                mounam ga
+                ........naa ventapadi............
 
-      lolo ekantam naa chuttu allina lokam nake sontam antunna
-      vinnara nenu naa needa iddarame chalantunna rakudadhu inkevarina 
-        </p>
+                lolo ekantam naa chuttu allina lokam nake sontam antunna
+                vinnara nenu naa needa iddarame chalantunna rakudadhu inkevarina
+
+                amma vodilo monna andani asalato ninna ento orristu undi jabilli
+                anta dooranunna vennalaga chentana unta antu ooyalalupindi jolali...
+                tare rare rarere
+            </p>
         </>
     )
 }
