205eb9b499d705c2d76b28c0fbe329800153f041
Rajitha-Kilaru <tirupathi2219@gmail.com>
2023-07-22 10:32:20 +0530

    auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,43 @@
+import axios from "axios"
+const { Auth } = require("two-step-auth");
+import { useEffect, useState } from "react"
+
 export default function Home() {
+    const [data,setData] = useState([])
+    const [email, setEmail] = useState('')
+        
+useEffect(() =>{
+    const fetchData = async () => {
+        try{
+            const products = await axios.get('https://fakestoreapi.com/products')
+            setData(products.data)
+        }
+        catch(error){
+            console.log('16::', error);
+        }
+    }     
+    fetchData() 
+
+},[])
+ const handleEmail = async () => {
+    console.log('25 0::', email)
+        try{
+            const res = await Auth(email)
+            console.log('25 1::', res)
+        }
+        catch(error){
+            console.log('29::',error);
+        }
+
+ }
+           
     return (
-        <h1>Home Page</h1>
+        <>
+        <h1 >hello </h1>
+        <input type='email' onChange={(e) => setEmail(e.target.value)}/> <button onClick = {handleEmail}>Send OTP</button>
+        {
+            data?.map((item :any) => <li>{item.title}</li>)
+        }
+        </>
     )
 }
