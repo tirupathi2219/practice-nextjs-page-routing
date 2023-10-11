f83b31fa5f0ceea18b0dc38f9210a0e64d78bed3
unknown <tirupathi2219@gmail.com>
2023-10-11 14:26:28 +0530

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,41 +3,40 @@ const { Auth } = require("two-step-auth");
 import { useEffect, useState } from "react"
 
 export default function Home() {
-    const [data,setData] = useState([])
+    const [data, setData] = useState([])
     const [email, setEmail] = useState('')
-        
-useEffect(() =>{
-    const fetchData = async () => {
-        try{
-            const products = await axios.get('https://fakestoreapi.com/products')
-            setData(products.data)
-        }
-        catch(error){
-            console.log('16::', error);
-        }
-    }     
-    fetchData() 
 
-},[])
- const handleEmail = async () => {
-    console.log('25 0::', email)
-        try{
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const products = await axios.get('https://fakestoreapi.com/products')
+                setData(products.data)
+            }
+            catch (error) {
+                console.log('16::', error);
+            }
+        }
+        fetchData()
+
+    }, [])
+    const handleEmail = async () => {
+        console.log('25 0::', email)
+        try {
             const res = await Auth(email)
             console.log('25 1::', res)
         }
-        catch(error){
-            console.log('29::',error);
+        catch (error) {
+            console.log('29::', error);
         }
 
- }
-           
+    }
+
     return (
         <>
-        <h1 >hello </h1>
-        <input type='email' onChange={(e) => setEmail(e.target.value)}/> <button onClick = {handleEmail}>Send OTP</button>
-        {
-            data?.map((item :any) => <li>{item.title}</li>)
-        }
+            <input type='email' onChange={(e) => setEmail(e.target.value)} /> <button onClick={handleEmail}>Send OTP</button>
+            {
+                data?.map((item: any) => <li>{item.title}</li>)
+            }
         </>
     )
 }
